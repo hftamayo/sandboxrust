3c3db54d0821d9dac637ecaa8e031966a680a766
hftamayo <hftamayo@gmail.com>
2023-01-05 14:49:43 -0600

    first three sets of exercises

---
+++ oneguessing_game/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("guess the number");
+
+    println!("please input your number");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("please type a number");
+
+    println!("your guessed: {guess}");
+    println!("the secret number is: {secret_number}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("you win!"),
+    }
+}
