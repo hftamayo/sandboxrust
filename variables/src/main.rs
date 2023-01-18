b9d8b80438b8c6dd56e8ca1a46092ea1bd292af5
hftamayo <hftamayo@gmail.com>
2023-01-18 14:58:26 -0600

    modify an unmutable var in a code segment

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of the variable is {x}");
-    x = 6;
-    println!("the value of the variable on a second moment is {x}");
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x + 2;
+        println!("The value of x in the inner scope is {x}");
+    }
+    println!("the value of the x is {x}");
 }
